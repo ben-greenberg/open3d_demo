521d2cbd4106cbf0acb8fee37d6502099977ad09
Ben Greenberg <ben.greenberg@swri.org>
2022-03-24 14:45:17 -0500

    Got camera calibrated

--- bash/start_capture.bash
+++ bash/start_capture.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 source /home/open3d_demo/open3d_demo_ws/devel/setup.bash
-rosservice call /start_reconstruction "tracking_frame: 'camera_depth_optical_frame'
+rosservice call /start_reconstruction "tracking_frame: 'realsense_optical_frame'
 relative_frame: 'world'
 translation_distance: 0.0
 rotational_distance: 0.0
@@ -11,4 +11,4 @@ tsdf_params:
   sdf_trunc: 0.02
   min_box_values: {x: 0.0, y: 0.0, z: 0.0}
   max_box_values: {x: 0.0, y: 0.0, z: 0.0}
-rgbd_params: {depth_scale: 1000.0, depth_trunc: 0.75, convert_rgb_to_intensity: false}"
+rgbd_params: {depth_scale: 1000.0, depth_trunc: 0.5, convert_rgb_to_intensity: false}"
